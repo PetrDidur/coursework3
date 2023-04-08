e2e37f95cbf2bb659e959da5e53c28499edd8391
PetrDidur <pdidur@yandex.ru>
2023-04-08 21:53:42 +0300

    main part

--- main.py
+++ main.py
@@ -4,12 +4,23 @@ if __name__ == '__main__':
     last_five_sorted = sort_by_date()
 
     for operation in last_five_sorted:
-        #print(operation)
-        result = ''
         date_actual = get_date(operation)
         description = operation['description']
-        card_number = get_operation(operation)
-        print(card_number)
+        sender = get_operation(operation)
+        receiver = get_receiver(operation)
+        amount = get_amount(operation)
+        currency = get_currency(operation)
+        print(f'{date_actual} {description}\n'
+              f'{sender} -> {receiver}\n'
+              f'{amount} {currency}\n')
+        print()
+
+
+
+
+
+
+
 
 
 
--- utlis.py
+++ utlis.py
@@ -32,7 +32,8 @@ def get_operation(operation):
         blocks = [card_number[i:i + 4] for i in range(0, len(card_number), 4)]
         blocks_str = "".join(blocks)
         masked_number = blocks_str[:6] + '*'*(len(blocks_str) - 10) + blocks_str[-4:]
-        return f"{card} {masked_number}"
+        masked_number_final = " ".join([masked_number[i:i+4] for i in range(0, len(masked_number),4)])
+        return f"{card} {masked_number_final}"
     elif "Visa" in from_data:
         from_data_actual = from_data.split(" ")
         card = from_data_actual[0] + " " + from_data_actual[1]
@@ -40,7 +41,41 @@ def get_operation(operation):
         blocks = [card_number[i:i + 4] for i in range(0, len(card_number), 4)]
         blocks_str = "".join(blocks)
         masked_number = blocks_str[:6] + '*' * (len(blocks_str) - 10) + blocks_str[-4:]
-        return f"{card} {masked_number}"
+        masked_number_final = " ".join([masked_number[i:i + 4] for i in range(0, len(masked_number), 4)])
+        return f"{card} {masked_number_final}"
+    elif "Счет" in from_data:
+        from_data_actual = from_data.split(" ")
+        card = from_data_actual[0]
+        card_number = from_data_actual[1]
+        blocks = [card_number[i:i + 4] for i in range(0, len(card_number), 4)]
+        blocks_str = "".join(blocks)
+        masked_number = blocks_str[:6] + '*' * (len(blocks_str) - 10) + blocks_str[-4:]
+        masked_number_final = " ".join([masked_number[i:i + 4] for i in range(0, len(masked_number), 4)])
+        return f"{card} {masked_number_final}"
+
+
+def get_receiver(operation):
+    receiver_data = operation.get('to').split(' ')
+    receiver = receiver_data[0]
+    receiver_num = receiver_data[1]
+    masked_num = "*" * 2 + receiver_num[-4:]
+    return f"{receiver} {masked_num}"
+
+
+def get_amount(operation):
+    amount_data = operation['operationAmount']['amount']
+    return amount_data
+
+
+def get_currency(operation):
+    currency = operation['operationAmount']['currency']['name']
+    return currency
+
+
+
+
+
+
 
 
 
